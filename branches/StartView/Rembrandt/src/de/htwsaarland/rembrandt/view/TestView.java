257bb11e7eb3b6c792e758c1fd7fbce7d9d4b247
godfather1408@gmail.com <godfather1408@gmail.com@175e4b30-34eb-ad61-8188-71c27a4c5039>
2012-08-20 08:45:30 +0000

    Test the commit function

---
+++ branches/StartView/Rembrandt/src/de/htwsaarland/rembrandt/view/TestView.java
@@ -0,0 +1,13 @@
+package de.htwsaarland.rembrandt.view;
+
+public class TestView {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
