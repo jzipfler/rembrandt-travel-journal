b0eb11e93fcd38975546542d657280f57bc0b98c
godfather1408@gmail.com <godfather1408@gmail.com@175e4b30-34eb-ad61-8188-71c27a4c5039>
2012-08-31 18:20:22 +0000

    Schriftgröße geändert

--- branches/Wizzard/Rembrandt/src/de/htwds/rembrandt/view/ViewWizzard.java
+++ branches/Wizzard/Rembrandt/src/de/htwds/rembrandt/view/ViewWizzard.java
@@ -107,7 +107,7 @@ public class ViewWizzard extends JFrame {
 		lblHeadLine.setMaximumSize(new Dimension(67, 20));
 		lblHeadLine.setHorizontalAlignment(SwingConstants.CENTER);
 		lblHeadLine.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblHeadLine.setFont(new Font("Arial", Font.BOLD, 14));
+		lblHeadLine.setFont(new Font("Arial", Font.BOLD, 15));
 		pnlViewContent.add(lblHeadLine, BorderLayout.NORTH);
 		
 		JPanel pnlContainer = new JPanel();
@@ -116,7 +116,7 @@ public class ViewWizzard extends JFrame {
 		
 		JTextPane txtpnExplanation = new JTextPane();
 		pnlContainer.add(txtpnExplanation);
-		txtpnExplanation.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtpnExplanation.setFont(new Font("Arial", Font.PLAIN, 13));
 		txtpnExplanation.setSelectedTextColor(Color.BLACK);
 		txtpnExplanation.setSelectionColor(Color.WHITE);
 		txtpnExplanation.setText("Dieser Wizzard wird Ihnen helfen eine neue Reise anzulegen.\r\n\r\nDazu werden Ihnen verschiedene Fragen, ihre Reise betreffend gestellt. \r\nSie werden nach Ihrem:\r\n\r\n\t- Datum\r\n\t- Reiseland\r\n\t- Ort\r\n\t- usw.\r\n\r\ngefragt. \r\nDiese Angaben dienen ebenfalls dazu den Reiseeintrag sp\u00E4ter wieder zu finden. Daher sollten die Angaben m\u00F6glichst pr\u00E4zise sein.");
@@ -154,24 +154,24 @@ public class ViewWizzard extends JFrame {
 				FormFactory.DEFAULT_ROWSPEC,}));
 		
 		JLabel lblLocation = new JLabel("Land- und Ortsangabe:");
-		lblLocation.setFont(new Font("Arial", Font.BOLD, 11));
+		lblLocation.setFont(new Font("Arial", Font.BOLD, 13));
 		pnlQuestion.add(lblLocation, "4, 2");
 		
 		JLabel lblCountry = new JLabel("In welches Land reisen Sie?");
-		lblCountry.setFont(new Font("Arial", Font.PLAIN, 11));
+		lblCountry.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlQuestion.add(lblCountry, "4, 4");
 		
 		JLabel lblCity = new JLabel("Name des Ortes Ihres Aufenthalts?");
-		lblCity.setFont(new Font("Arial", Font.PLAIN, 11));
+		lblCity.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlQuestion.add(lblCity, "8, 4");
 		
 		txtInputCountry = new JTextField();
-		txtInputCountry.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtInputCountry.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlQuestion.add(txtInputCountry, "4, 6");
 		txtInputCountry.setColumns(10);
 		
 		txtInputCity = new JTextField();
-		txtInputCity.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtInputCity.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlQuestion.add(txtInputCity, "8, 6, fill, default");
 		txtInputCity.setColumns(10);
 		
@@ -185,7 +185,7 @@ public class ViewWizzard extends JFrame {
 		
 		JLabel lblHeadLine_1 = new JLabel("Reiseart und Datum");
 		lblHeadLine_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblHeadLine_1.setFont(new Font("Arial", Font.BOLD, 14));
+		lblHeadLine_1.setFont(new Font("Arial", Font.BOLD, 15));
 		pnlViewContent_1.add(lblHeadLine_1, BorderLayout.NORTH);
 		
 		JPanel pnlContainer_1 = new JPanel();
@@ -219,29 +219,29 @@ public class ViewWizzard extends JFrame {
 				RowSpec.decode("14px"),}));
 		
 		JLabel lblDateSection = new JLabel("Datumsangaben:");
-		lblDateSection.setFont(new Font("Arial", Font.BOLD, 11));
+		lblDateSection.setFont(new Font("Arial", Font.BOLD, 13));
 		pnlContainer_1.add(lblDateSection, "4, 2, left, top");
 		
 		JLabel lblDate = new JLabel("Datum des Reisebeginns?");
-		lblDate.setFont(new Font("Arial", Font.PLAIN, 11));
+		lblDate.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_1.add(lblDate, "4, 4, left, top");
 		
 		JLabel lblEndDate = new JLabel("Das Ende der Reise?");
-		lblEndDate.setFont(new Font("Arial", Font.PLAIN, 11));
+		lblEndDate.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_1.add(lblEndDate, "8, 4, left, top");
 		
 		txtDate = new JTextField();
-		txtDate.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtDate.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_1.add(txtDate, "4, 6, fill, top");
 		txtDate.setColumns(10);
 		
 		txtEndDate = new JTextField();
-		txtEndDate.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtEndDate.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_1.add(txtEndDate, "8, 6, fill, top");
 		txtEndDate.setColumns(10);
 		
 		JLabel lblTravelOptions = new JLabel("Reiseart:");
-		lblTravelOptions.setFont(new Font("Arial", Font.BOLD, 11));
+		lblTravelOptions.setFont(new Font("Arial", Font.BOLD, 13));
 		pnlContainer_1.add(lblTravelOptions, "4, 8, left, top");
 		
 		JPanel pnlSubContainer = new JPanel();
@@ -254,18 +254,18 @@ public class ViewWizzard extends JFrame {
 				FormFactory.DEFAULT_ROWSPEC,}));
 		
 		JComboBox cboOptions = new JComboBox();
-		cboOptions.setFont(new Font("Arial", Font.PLAIN, 11));
+		cboOptions.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlSubContainer.add(cboOptions, "2, 2");
 		cboOptions.setSize(new Dimension(50, 20));
 		cboOptions.setPreferredSize(new Dimension(50, 20));
 		cboOptions.setModel(new DefaultComboBoxModel(new String[] {"Auto", "Bus", "Fahrrad", "Flugzeug", "Motorrad", "Schiff", "Zug"}));
 		
 		JLabel lblHeadlineNote = new JLabel("Hinweise:");
-		lblHeadlineNote.setFont(new Font("Arial", Font.BOLD, 11));
+		lblHeadlineNote.setFont(new Font("Arial", Font.BOLD, 13));
 		pnlContainer_1.add(lblHeadlineNote, "4, 12, left, top");
 		
 		JLabel lblNote = new JLabel("Die hier angegebene Reiseart beschr\u00E4nkt sich auf den Hauptweg. Ben\u00F6tigte Transfers werden hier nicht ber\u00FCcksichtigt.");
-		lblNote.setFont(new Font("Arial", Font.PLAIN, 11));
+		lblNote.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_1.add(lblNote, "4, 14, 5, 1, left, top");
 		
 		pnlThirdView = new JPanel();
@@ -278,7 +278,7 @@ public class ViewWizzard extends JFrame {
 		
 		JLabel lblHeadLine_2 = new JLabel("Details zur Reiseroute");
 		pnlViewContent_2.add(lblHeadLine_2, BorderLayout.NORTH);
-		lblHeadLine_2.setFont(new Font("Arial", Font.BOLD, 14));
+		lblHeadLine_2.setFont(new Font("Arial", Font.BOLD, 15));
 		lblHeadLine_2.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JPanel pnlContainer_2 = new JPanel();
@@ -312,46 +312,46 @@ public class ViewWizzard extends JFrame {
 				FormFactory.DEFAULT_ROWSPEC,}));
 		
 		JLabel lblArrivalTxt = new JLabel("Anreise:");
-		lblArrivalTxt.setFont(new Font("Arial", Font.BOLD, 11));
+		lblArrivalTxt.setFont(new Font("Arial", Font.BOLD, 13));
 		pnlContainer_2.add(lblArrivalTxt, "3, 2");
 		
 		JLabel lblStartArrival = new JLabel("Start ?");
-		lblStartArrival.setFont(new Font("Arial", Font.PLAIN, 11));
+		lblStartArrival.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_2.add(lblStartArrival, "3, 4");
 		
 		JLabel lblDestinationArrival = new JLabel("Ziel ?");
-		lblDestinationArrival.setFont(new Font("Arial", Font.PLAIN, 11));
+		lblDestinationArrival.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_2.add(lblDestinationArrival, "7, 4");
 		
 		txtStartArrivalInput = new JTextField();
-		txtStartArrivalInput.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtStartArrivalInput.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_2.add(txtStartArrivalInput, "3, 6, fill, default");
 		txtStartArrivalInput.setColumns(10);
 		
 		txtDestinationArrivalInput = new JTextField();
-		txtDestinationArrivalInput.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtDestinationArrivalInput.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_2.add(txtDestinationArrivalInput, "7, 6, fill, default");
 		txtDestinationArrivalInput.setColumns(10);
 		
 		JLabel lblDeparture = new JLabel("Abreise:");
-		lblDeparture.setFont(new Font("Arial", Font.BOLD, 11));
+		lblDeparture.setFont(new Font("Arial", Font.BOLD, 13));
 		pnlContainer_2.add(lblDeparture, "3, 12");
 		
 		JLabel lblDepartureStart = new JLabel("Start ?");
-		lblDepartureStart.setFont(new Font("Arial", Font.PLAIN, 11));
+		lblDepartureStart.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_2.add(lblDepartureStart, "3, 14");
 		
 		JLabel lblDepartureDestination = new JLabel("Ziel ?");
-		lblDepartureDestination.setFont(new Font("Arial", Font.PLAIN, 11));
+		lblDepartureDestination.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_2.add(lblDepartureDestination, "7, 14");
 		
 		txtDepartureStartInput = new JTextField();
-		txtDepartureStartInput.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtDepartureStartInput.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_2.add(txtDepartureStartInput, "3, 16, fill, default");
 		txtDepartureStartInput.setColumns(10);
 		
 		txtDepartureDestinationInput = new JTextField();
-		txtDepartureDestinationInput.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtDepartureDestinationInput.setFont(new Font("Arial", Font.PLAIN, 13));
 		pnlContainer_2.add(txtDepartureDestinationInput, "7, 16, fill, default");
 		txtDepartureDestinationInput.setColumns(10);
 		
@@ -368,7 +368,7 @@ public class ViewWizzard extends JFrame {
 				RowSpec.decode("23px"),}));
 		
 		btnNext = new JButton("Weiter");
-		btnNext.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnNext.setFont(new Font("Arial", Font.PLAIN, 13));
 		btnNext.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (cardNumber < THIRD){
@@ -400,7 +400,7 @@ public class ViewWizzard extends JFrame {
 		});
 		
 		btnBack = new JButton("Abbrechen");
-		btnBack.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnBack.setFont(new Font("Arial", Font.PLAIN, 13));
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (cardNumber > FIRST){
