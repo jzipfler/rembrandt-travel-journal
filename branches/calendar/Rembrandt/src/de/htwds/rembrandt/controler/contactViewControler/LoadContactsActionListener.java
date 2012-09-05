492024a9a9860554a1b2e9932e756bcdc256a146
zipfler.j@gmail.com <zipfler.j@gmail.com@175e4b30-34eb-ad61-8188-71c27a4c5039>
2012-09-05 09:59:41 +0000

    Merge with stable

--- branches/calendar/Rembrandt/src/de/htwds/rembrandt/controler/contactViewControler/LoadContactsActionListener.java
+++ branches/calendar/Rembrandt/src/de/htwds/rembrandt/controler/contactViewControler/LoadContactsActionListener.java
@@ -3,6 +3,7 @@ package de.htwds.rembrandt.controler.contactViewControler;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import de.htwds.rembrandt.view.ViewContactDetails;
 import de.htwds.rembrandt.view.ViewContacts;
 import de.htwds.rembrandt.view.ViewMain;
 
@@ -16,13 +17,25 @@ public class LoadContactsActionListener implements ActionListener {
 		this.viewContacts = viewContacts;
 	}	
 	
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		
+	private int decideChangeOrNot(){
+		ViewContactDetails viewContactDetails = viewContacts.getViewContactDetails();
+		if ( viewContactDetails.getBtnUnlock().getText() == viewContactDetails.STD_BUTTON_UNLOCK_LOCK )
+			return -1;
+		return 0;
+	}
+	
+	private void performChange(){
 		ViewMain viewMain = viewContacts.getParentFrame();
 		viewMain.getCurrentContentPanel().removeAll();
 		viewMain.getCurrentContentPanel().add(viewContacts);
 		viewMain.getCurrentContentPanel().updateUI();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		if ( decideChangeOrNot() == 0 )
+			performChange();
 	}
 
 }
