6a27be130d8fcf4147f1205dd5a0573aaca3e5bc
zipfler.j@gmail.com <zipfler.j@gmail.com@175e4b30-34eb-ad61-8188-71c27a4c5039>
2012-09-07 09:13:09 +0000

    Create a datastruture and create folder structure on filesystem.

---
+++ branches/create-datastructur-on-filesystem/Rembrandt/src/de/htwds/rembrandt/model/Contact.java
@@ -0,0 +1,470 @@
+package de.htwds.rembrandt.model;
+
+import javax.swing.Icon;
+
+import de.htwds.rembrandt.exception.ContactException;
+
+public class Contact implements Comparable<Contact> {
+
+	private static final String SPACE = " ";
+	
+	private String firstName;
+	private String lastName;
+	private String postfach;
+	private String city;
+//	private int zipPlz;
+	private String zipPlz;
+	private String stateProvinz;
+	private String country;
+	private String homepage;
+	private String job;
+	private String company;
+	private String department;
+	private String office;
+	private String spouses;
+	private String privatMail;
+	private String businessMail;
+//	private int privatPhone;
+//	private int businessPhone;
+	private String privatPhone;
+	private String businessPhone;
+	private String privatAdress;
+	private String businessAdress;
+	private String notices;
+	
+	private Icon picture;
+	
+	/**
+	 * Minimum constructor that creates a contact only with his first an last name.
+	 * @param firstname
+	 * @param lastname
+	 * @throws ContactException
+	 */
+	public Contact( String firstName, String lastName ) throws ContactException {
+		ContactException.firstNameEmpty( firstName );
+		ContactException.lastNameEmpty( lastName );
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	/**
+	 * Constructor with all parameters needed for that contact
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param postfach
+	 * @param city
+	 * @param zipPlz
+	 * @param stateProvinz
+	 * @param country
+	 * @param homepage
+	 * @param job
+	 * @param company
+	 * @param department
+	 * @param office
+	 * @param spouses
+	 * @param privatMail
+	 * @param businessMail
+	 * @param privatPhone
+	 * @param businessPhone
+	 * @param privatAdress
+	 * @param businessAdress
+	 * @param notices
+	 * @param picture
+	 * @throws ContactException
+	 */
+	public Contact(	String firstName, String lastName, String postfach,
+					String city, String zipPlz, String stateProvinz, String country,
+					String homepage, String job, String company, String department,
+					String office, String spouses, String privatMail,
+					String businessMail, String privatPhone, String businessPhone,
+					String privatAdress, String businessAdress, String notices, Icon picture) throws ContactException {
+		
+		ContactException.firstNameEmpty( firstName );
+		ContactException.lastNameEmpty( lastName );
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.postfach = postfach;
+		this.city = city;
+		this.zipPlz = zipPlz;
+		this.stateProvinz = stateProvinz;
+		this.country = country;
+		this.homepage = homepage;
+		this.job = job;
+		this.company = company;
+		this.department = department;
+		this.office = office;
+		this.spouses = spouses;
+		this.privatMail = privatMail;
+		this.businessMail = businessMail;
+		this.privatPhone = privatPhone;
+		this.businessPhone = businessPhone;
+		this.privatAdress = privatAdress;
+		this.businessAdress = businessAdress;
+		this.notices = notices;
+		this.picture = picture;
+	}
+
+	public String toString() {
+		return ( lastName + SPACE + firstName );
+	}
+
+	@Override
+	public int compareTo(Contact arg0) {
+		return this.toString().compareTo( arg0.toString() );
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result
+//				+ ((firstName == null) ? 0 : firstName.hashCode());
+//		result = prime * result
+//				+ ((lastName == null) ? 0 : lastName.hashCode());
+//		return result;
+//		
+//		Created from Eclipse
+		return this.toString().hashCode();
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contact other = (Contact) obj;
+		if (firstName == null)
+			if (other.firstName != null)
+				return false;
+		if (lastName == null)
+			if (other.lastName != null)
+				return false;
+		if ( other.toString().equalsIgnoreCase(this.toString()) )
+			return true;
+		return false;
+	}
+	
+	/*
+	 * ######################################################################
+	 *  Now only getter and setter
+	 * ######################################################################
+	 */
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @return the postfach
+	 */
+	public String getPostfach() {
+		return postfach;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @return the zipPlz
+	 */
+	public String getZipPlz() {
+		return zipPlz;
+	}
+
+	/**
+	 * @return the stateProvinz
+	 */
+	public String getStateProvinz() {
+		return stateProvinz;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+
+	/**
+	 * @return the homepage
+	 */
+	public String getHomepage() {
+		return homepage;
+	}
+
+	/**
+	 * @return the job
+	 */
+	public String getJob() {
+		return job;
+	}
+
+	/**
+	 * @return the company
+	 */
+	public String getCompany() {
+		return company;
+	}
+
+	/**
+	 * @return the department
+	 */
+	public String getDepartment() {
+		return department;
+	}
+
+	/**
+	 * @return the office
+	 */
+	public String getOffice() {
+		return office;
+	}
+
+	/**
+	 * @return the spouses
+	 */
+	public String getSpouses() {
+		return spouses;
+	}
+
+	/**
+	 * @return the privatMail
+	 */
+	public String getPrivatMail() {
+		return privatMail;
+	}
+
+	/**
+	 * @return the businessMail
+	 */
+	public String getBusinessMail() {
+		return businessMail;
+	}
+
+	/**
+	 * @return the privatPhone
+	 */
+	public String getPrivatPhone() {
+		return privatPhone;
+	}
+
+	/**
+	 * @return the businessPhone
+	 */
+	public String getBusinessPhone() {
+		return businessPhone;
+	}
+
+	/**
+	 * @return the privatAdress
+	 */
+	public String getPrivatAdress() {
+		return privatAdress;
+	}
+
+	/**
+	 * @return the businessAdress
+	 */
+	public String getBusinessAdress() {
+		return businessAdress;
+	}
+
+	/**
+	 * @return the notices
+	 */
+	public String getNotices() {
+		return notices;
+	}
+	
+	/**
+	 * 
+	 * @return the picture
+	 */
+	public Icon getPicture() {
+		return picture;
+	}
+
+	/*
+	 * ##################################################################
+	 * Setter
+	 * ##################################################################
+	 */
+	
+	/**
+	 * @param firstName the firstName to set
+	 * @throws ContactException
+	 */
+	public void setFirstName(String firstName) throws ContactException {
+		ContactException.firstNameEmpty( firstName );
+		
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 * @throws ContactException
+	 */
+	public void setLastName(String lastName) throws ContactException {
+		ContactException.lastNameEmpty( lastName );
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @param postfach the postfach to set
+	 */
+	public void setPostfach(String postfach) {
+		this.postfach = postfach;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @param zipPlz the zipPlz to set
+	 */
+	public void setZipPlz(String zipPlz) {
+		this.zipPlz = zipPlz;
+	}
+
+	/**
+	 * @param stateProvinz the stateProvinz to set
+	 */
+	public void setStateProvinz(String stateProvinz) {
+		this.stateProvinz = stateProvinz;
+	}
+
+	/**
+	 * @param country the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	/**
+	 * @param homepage the homepage to set
+	 */
+	public void setHomepage(String homepage) {
+		this.homepage = homepage;
+	}
+
+	/**
+	 * @param job the job to set
+	 */
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	/**
+	 * @param company the company to set
+	 */
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	/**
+	 * @param department the department to set
+	 */
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	/**
+	 * @param office the office to set
+	 */
+	public void setOffice(String office) {
+		this.office = office;
+	}
+
+	/**
+	 * @param spouses the spouses to set
+	 */
+	public void setSpouses(String spouses) {
+		this.spouses = spouses;
+	}
+
+	/**
+	 * @param privatMail the privatMail to set
+	 */
+	public void setPrivatMail(String privatMail) {
+		this.privatMail = privatMail;
+	}
+
+	/**
+	 * @param businessMail the businessMail to set
+	 */
+	public void setBusinessMail(String businessMail) {
+		this.businessMail = businessMail;
+	}
+
+	/**
+	 * @param privatPhone the privatPhone to set
+	 */
+	public void setPrivatPhone(String privatPhone) {
+		this.privatPhone = privatPhone;
+	}
+
+	/**
+	 * @param businessPhone the businessPhone to set
+	 */
+	public void setBusinessPhone(String businessPhone) {
+		this.businessPhone = businessPhone;
+	}
+
+	/**
+	 * @param privatAdress the privatAdress to set
+	 */
+	public void setPrivatAdress(String privatAdress) {
+		this.privatAdress = privatAdress;
+	}
+
+	/**
+	 * @param businessAdress the businessAdress to set
+	 */
+	public void setBusinessAdress(String businessAdress) {
+		this.businessAdress = businessAdress;
+	}
+
+	/**
+	 * @param notices the notices to set
+	 */
+	public void setNotices(String notices) {
+		this.notices = notices;
+	}
+	
+	/**
+	 * 
+	 * @param picture the picture to set
+	 */
+	public void setPicture( Icon picture ) {
+		this.picture = picture;
+	}
+
+}
---
+++ branches/create-datastructur-on-filesystem/Rembrandt/src/de/htwds/rembrandt/view/ViewPhotoAlbumDetails.java
@@ -0,0 +1,121 @@
+package de.htwds.rembrandt.view;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
+import net.miginfocom.swing.MigLayout;
+
+/**
+ * Photo Album View
+ * Details
+ * 
+ * @author sFey
+ */
+public class ViewPhotoAlbumDetails extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+    private JTextField txtFileName;
+    private JTextField txtFileDate;
+    private JTextField txtComment;
+
+    public ViewPhotoAlbumDetails() {
+        setLayout(new BorderLayout(0, 0));
+        
+        JPanel pnlPhotoArea = new JPanel();
+        add(pnlPhotoArea, BorderLayout.CENTER);
+        pnlPhotoArea.setLayout(new BorderLayout(0, 0));
+        
+        JButton btnPhotoBack = new JButton("<");
+        btnPhotoBack.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlPhotoArea.add(btnPhotoBack, BorderLayout.WEST);
+        
+        JButton btnPhotoForward = new JButton(">");
+        btnPhotoForward.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlPhotoArea.add(btnPhotoForward, BorderLayout.EAST);
+        
+        JLabel lblCurrentPhoto = new JLabel("current");
+        lblCurrentPhoto.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlPhotoArea.add(lblCurrentPhoto, BorderLayout.CENTER);
+        
+        JPanel pnlInformation = new JPanel();
+        add(pnlInformation, BorderLayout.EAST);
+        
+        JLabel lblFileName = new JLabel("Name");
+        lblFileName.setFont(new Font("Arial", Font.PLAIN, 13));
+        
+        txtFileName = new JTextField();
+        txtFileName.setFont(new Font("Arial", Font.PLAIN, 13));
+        txtFileName.setColumns(10);
+        
+        JLabel lblFileDate = new JLabel("Date");
+        lblFileDate.setFont(new Font("Arial", Font.PLAIN, 13));
+        
+        txtFileDate = new JTextField();
+        txtFileDate.setFont(new Font("Arial", Font.PLAIN, 13));
+        txtFileDate.setColumns(10);
+        
+        JLabel lblComment = new JLabel("Comment");
+        lblComment.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlInformation.setLayout(new MigLayout("", "[87px,grow]", "[14px][20px][14px][20px][14px][22px,grow]"));
+        pnlInformation.add(lblFileName, "cell 0 0,alignx left,aligny top");
+        pnlInformation.add(txtFileName, "cell 0 1,alignx right,aligny top");
+        pnlInformation.add(lblFileDate, "cell 0 2,alignx left,aligny top");
+        pnlInformation.add(txtFileDate, "cell 0 3,alignx right,aligny top");
+        pnlInformation.add(lblComment, "cell 0 4,alignx left,aligny top");
+        
+        JScrollPane spnComment = new JScrollPane();
+        spnComment.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        pnlInformation.add(spnComment, "cell 0 5,growy");
+        
+        txtComment = new JTextField();
+        txtComment.setFont(new Font("Arial", Font.PLAIN, 13));
+        spnComment.setViewportView(txtComment);
+        txtComment.setColumns(10);
+        
+        JPanel pnlThumbnail = new JPanel();
+        add(pnlThumbnail, BorderLayout.SOUTH);
+        pnlThumbnail.setLayout(new GridLayout(0, 8, 0, 0));
+        
+        JButton btnThumbnailBack = new JButton("<");
+        btnThumbnailBack.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlThumbnail.add(btnThumbnailBack);
+        
+        JLabel lblThumbnail1 = new JLabel("1");
+        lblThumbnail1.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlThumbnail.add(lblThumbnail1);
+        
+        JLabel lblThumbnail2 = new JLabel("2");
+        lblThumbnail2.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlThumbnail.add(lblThumbnail2);
+        
+        JLabel lblThumbnail3 = new JLabel("3");
+        lblThumbnail3.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlThumbnail.add(lblThumbnail3);
+        
+        JLabel lblThumbnail4 = new JLabel("4");
+        lblThumbnail4.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlThumbnail.add(lblThumbnail4);
+        
+        JLabel lblThumbnail5 = new JLabel("5");
+        lblThumbnail5.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlThumbnail.add(lblThumbnail5);
+        
+        JButton btnThumbnailForward = new JButton(">");
+        btnThumbnailForward.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlThumbnail.add(btnThumbnailForward);
+        
+        JButton btnOverview = new JButton("Overview");
+        btnOverview.setFont(new Font("Arial", Font.PLAIN, 13));
+        pnlThumbnail.add(btnOverview);
+
+    }
+
+}
