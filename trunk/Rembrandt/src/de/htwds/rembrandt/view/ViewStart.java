704cddef033e05911343ce4eb8498ce16fcc1c9c
zipfler.j@gmail.com <zipfler.j@gmail.com@175e4b30-34eb-ad61-8188-71c27a4c5039>
2012-09-10 18:48:01 +0000

    Merge with branch: create-datastructure-on-filesystem

---
+++ trunk/Rembrandt/src/de/htwds/rembrandt/view/ViewStart.java
@@ -0,0 +1,246 @@
+package de.htwds.rembrandt.view;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
+
+/**
+ * 
+ * @author Jan Zipfler
+ * @version Ported the BetaView in Stable - 20120910
+ */
+public class ViewStart extends JPanel {
+
+	private ViewWrapperWindow wrapperView;
+
+
+	private JPanel panelViewStart;
+	private JTextField txtFilter;
+	private JTable table;
+	private DefaultTableModel tableModel;
+	private String[] tableTitles;
+	private Vector<String> tableDataVector;
+	private JButton btnRemoveJourney;
+	private JTextField txtTarget;
+	private JTextField txtDeparture;
+	private JTextField txtCountry;
+	private JTextField txtJounreyEnd;
+	private JPanel infoAboutTravelPane;
+
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewStart( ViewWrapperWindow wrapperView ) {
+//			setTitle("Mainmenu Travel Journal Software");
+		setName("Mainmenu Journey Log");
+		setMinimumSize(new Dimension(640, 480));
+		setBounds(100, 100, 640, 480);
+
+		panelViewStart = new JPanel();
+		this.panelViewStart.setPreferredSize(new Dimension(607, 376));
+		this.panelViewStart.setMinimumSize(new Dimension(440, 440));
+		panelViewStart.setBorder(new EmptyBorder(5, 5, 5, 5));
+		panelViewStart.setLayout(new BorderLayout(0, 0));
+//			setContentPane(contentPane);
+
+		JPanel quicklunchPanel = new JPanel();
+		quicklunchPanel.setPreferredSize(new Dimension(180, 10));
+		quicklunchPanel.setMinimumSize(new Dimension(135, 10));
+		quicklunchPanel.setMaximumSize(new Dimension(135, 32767));
+		panelViewStart.add(quicklunchPanel, BorderLayout.WEST);
+		
+		JButton btnCreateJourney = new JButton("Reise anlegen");
+		btnCreateJourney.setFont(new Font("Arial", Font.BOLD, 13));
+		btnCreateJourney.setPreferredSize(new Dimension(150, 25));
+		btnCreateJourney.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		quicklunchPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		quicklunchPanel.add(btnCreateJourney);
+		
+		btnRemoveJourney = new JButton("Reise entfernen");
+		this.btnRemoveJourney.setFont(new Font("Arial", Font.BOLD, 13));
+		btnRemoveJourney.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnRemoveJourney.setPreferredSize(new Dimension(150, 25));
+		btnRemoveJourney.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				removeTableRow(null);
+			}
+		});
+		btnRemoveJourney.setEnabled(false);
+		quicklunchPanel.add(btnRemoveJourney);
+		
+		JPanel initialContentPane = new JPanel();
+		panelViewStart.add(initialContentPane, BorderLayout.CENTER);
+		initialContentPane.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		initialContentPane.add(scrollPane);
+		
+		tableTitles = new String[]{ "Reise:" };
+		tableModel = new DefaultTableModel( tableTitles, 0);
+		table = new JTable( tableModel ){
+			/**
+			 * Ermöglicht Alighment = Rechts
+			 */
+			private static final long serialVersionUID = 1L;
+			DefaultTableCellRenderer defaultRendererTest = new DefaultTableCellRenderer(); {
+				defaultRendererTest.setHorizontalAlignment(SwingConstants.RIGHT);
+			}
+			public TableCellRenderer getCellRenderer(int arg0, int arg1){
+				return defaultRendererTest;
+			}
+		};
+		this.table.setFont(new Font("Arial", Font.PLAIN, 13));
+		table.setRowHeight(20);
+		table.setRowMargin(2);
+		table.setAutoCreateRowSorter(true);
+		table.setBorder(null);
+		table.setDragEnabled(true);
+		table.setFillsViewportHeight(true);
+		table.setRowSelectionAllowed(false);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setShowVerticalLines(false);
+		scrollPane.setViewportView(table);
+		
+		JPanel panel = new JPanel();
+		initialContentPane.add(panel, BorderLayout.NORTH);
+		FlowLayout fl_panel = new FlowLayout(FlowLayout.RIGHT, 5, 5);
+		fl_panel.setAlignOnBaseline(true);
+		panel.setLayout(fl_panel);
+		
+		JLabel lblReisenFiltern = new JLabel("Reisen filtern: ");
+		lblReisenFiltern.setFont(new Font("Arial", Font.BOLD, 13));
+		panel.add(lblReisenFiltern);
+		
+		txtFilter = new JTextField();
+		this.txtFilter.setFont(new Font("Arial", Font.PLAIN, 13));
+		panel.add(txtFilter);
+		txtFilter.setColumns(15);
+		
+		infoAboutTravelPane = new JPanel();
+		infoAboutTravelPane.setVisible(false);
+		infoAboutTravelPane.setPreferredSize(new Dimension(10, 180));
+		initialContentPane.add(infoAboutTravelPane, BorderLayout.SOUTH);
+		infoAboutTravelPane.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(50dlu;default):grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblInfo = new JLabel("Info");
+		lblInfo.setFont(new Font("Arial", Font.BOLD, 20));
+		infoAboutTravelPane.add(lblInfo, "4, 2");
+		
+		JLabel lblZiel = new JLabel("Ziel");
+		lblZiel.setFont(new Font("Arial", Font.BOLD, 13));
+		infoAboutTravelPane.add(lblZiel, "4, 4");
+		
+		JLabel lblAbfahrtszeitpunkt = new JLabel("Abfahrtszeitpunkt");
+		lblAbfahrtszeitpunkt.setFont(new Font("Arial", Font.BOLD, 13));
+		infoAboutTravelPane.add(lblAbfahrtszeitpunkt, "6, 4");
+		
+		txtTarget = new JTextField();
+		this.txtTarget.setFont(new Font("Arial", Font.PLAIN, 13));
+		txtTarget.setEditable(false);
+		infoAboutTravelPane.add(txtTarget, "4, 6, fill, default");
+		txtTarget.setColumns(10);
+		
+		txtDeparture = new JTextField();
+		this.txtDeparture.setFont(new Font("Arial", Font.PLAIN, 13));
+		txtDeparture.setEditable(false);
+		infoAboutTravelPane.add(txtDeparture, "6, 6, fill, default");
+		txtDeparture.setColumns(10);
+		
+		JLabel lblCountry = new JLabel("Land");
+		lblCountry.setFont(new Font("Arial", Font.BOLD, 13));
+		infoAboutTravelPane.add(lblCountry, "4, 10");
+		
+		JLabel lblGendertAm = new JLabel("Abreisezeitpunkt");
+		lblGendertAm.setFont(new Font("Arial", Font.BOLD, 13));
+		infoAboutTravelPane.add(lblGendertAm, "6, 10");
+		
+		txtCountry = new JTextField();
+		this.txtCountry.setFont(new Font("Arial", Font.PLAIN, 13));
+		txtCountry.setEditable(false);
+		infoAboutTravelPane.add(txtCountry, "4, 12, fill, default");
+		txtCountry.setColumns(10);
+		
+		txtJounreyEnd = new JTextField();
+		this.txtJounreyEnd.setFont(new Font("Arial", Font.PLAIN, 13));
+		txtJounreyEnd.setEditable(false);
+		infoAboutTravelPane.add(txtJounreyEnd, "6, 12, fill, default");
+		txtJounreyEnd.setColumns(10);
+	}
+	
+	void addTableRow( String item ){
+		//int size = tableModel.getColumnCount();
+		tableDataVector = new Vector<String>();
+		tableDataVector.add( item );
+		//tableDataVector.add( getCurrentDate() );
+		
+		tableModel.addRow(tableDataVector);
+
+		btnRemoveJourney.setEnabled(true);
+	}
+	
+	void removeTableRow( String item ){
+		int size = tableModel.getRowCount();
+		//int index = (int)(Math.random() * size);
+		tableModel.removeRow( size - 1 );
+		
+		btnRemoveJourney.setEnabled( size > 1 );	
+	}
+	
+	String getCurrentDate() {	
+		SimpleDateFormat formatter = new SimpleDateFormat ("dd.MM.yyyy 'um' HH:mm 'Uhr' ");
+		Date currentTime = new Date();
+		return formatter.format(currentTime).toString();
+	}
+	
+	public JPanel getViewStart(){
+		return panelViewStart;
+	}
+}
