d78c37c45867f7b2f994356e0147f2d59a1c2f97
zipfler.j@gmail.com <zipfler.j@gmail.com@175e4b30-34eb-ad61-8188-71c27a4c5039>
2012-09-12 12:56:59 +0000

    Merge with stable

--- Rembrandt/src/de/htwds/rembrandt/controler/contactViewControler/LoadCalendarActionListener.java
+++ Rembrandt/src/de/htwds/rembrandt/controler/contactViewControler/LoadCalendarActionListener.java
@@ -4,27 +4,32 @@ package de.htwds.rembrandt.controler.contactViewControler;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import de.htwds.rembrandt.view.ViewContacts;
 import de.htwds.rembrandt.view.ViewMain;
 import de.htwds.rembrandt.view.ViewCalendar;
-import de.htwds.rembrandt.view.ViewTravelinformation;
+
 
 public class LoadCalendarActionListener implements ActionListener {
-
 	
-	private ViewMain viewMain;
+
 	private ViewCalendar viewCalendar;
 	
-	public LoadCalendarActionListener(ViewMain viewMain){
-		this.viewMain = viewMain;
-		this.viewCalendar = new ViewCalendar();
-	}
+	public LoadCalendarActionListener(ViewMain viewMain ) {	
+		
+		this.viewCalendar = new ViewCalendar( viewMain );
+		
+	}	
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent e) {
+		
+		ViewMain viewMain = viewCalendar.getParentFrame();
 		viewMain.getCurrentContentPanel().removeAll();
 		viewMain.getCurrentContentPanel().add(viewCalendar);
 		viewCalendar.updateUI();
 	}
-
+	
+	
+	
+	
 }
