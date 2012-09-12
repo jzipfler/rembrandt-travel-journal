59e00eed873d3aa87bc77d4138acc766ad092495
zipfler.j@gmail.com <zipfler.j@gmail.com@175e4b30-34eb-ad61-8188-71c27a4c5039>
2012-09-12 11:12:46 +0000

    Added FolderPathController class - returns all folder paths in a static way. Modified related classes to create datasturcture.

--- Rembrandt/src/de/htwds/rembrandt/exception/DataStructureException.java
+++ Rembrandt/src/de/htwds/rembrandt/exception/DataStructureException.java
@@ -2,6 +2,10 @@ package de.htwds.rembrandt.exception;
 
 public class DataStructureException extends RuntimeException {
 
+	public static final String ERROR_EXISTS_AS_FILE = " - Existiert bereits als Datei.";
+	public static final String ERROR_CREATE_FOLDER = " - Dieser Ordner konnte nicht erstellt werden.";
+	
+	
 	public DataStructureException() {
 		super();
 	}
