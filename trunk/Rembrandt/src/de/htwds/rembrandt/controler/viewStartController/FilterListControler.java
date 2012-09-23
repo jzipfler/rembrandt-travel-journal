3c3008c81751f7a58a66ad3e9b060c0ef69eb998
zipfler.j@gmail.com <zipfler.j@gmail.com@175e4b30-34eb-ad61-8188-71c27a4c5039>
2012-09-23 16:34:49 +0000

    Merge with branch: filter-branch

---
+++ trunk/Rembrandt/src/de/htwds/rembrandt/controler/viewStartController/FilterListControler.java
@@ -0,0 +1,63 @@
+package de.htwds.rembrandt.controler.viewStartController;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+
+import de.htwds.rembrandt.controler.datastructure.GeneralInformationFromDiskControler;
+import de.htwds.rembrandt.exception.TravelToDiscException;
+import de.htwds.rembrandt.model.GeneralInformationModel;
+import de.htwds.rembrandt.view.ViewStart;
+
+public class FilterListControler {
+	
+	private ViewStart viewStart;
+	private GeneralInformationModel[] filtered;
+	
+	public FilterListControler(ViewStart viewStart){
+		this.viewStart = viewStart;
+		filtered = filter(viewStart.getGeneralInformationArray());
+		loadTableItems();
+	}
+	
+	private GeneralInformationModel[] filter(GeneralInformationModel[] actual){
+		ArrayList<GeneralInformationModel> list = new ArrayList<GeneralInformationModel>();
+		for(int i=0; i < actual.length;i++){
+			if(actual[i].getFolderName().contains(getFilter()) || getFilter().length() < 1)
+				list.add(actual[i]);
+		}
+		
+		if (!list.isEmpty()) {
+			return ((GeneralInformationModel[]) list.toArray(new GeneralInformationModel[list.size()]));
+		} else {
+			return null;
+		}
+	}
+	
+	private String getFilter(){
+		return viewStart.getTxtFilter().getText();
+	}
+	
+	
+	private void loadTableItems() {
+//		for(int i=0; i < viewStart.getTableModel().getRowCount(); i++){
+//			System.out.println(i + ".Reihe: " + viewStart.getTableModel().getValueAt(i, 0));
+//			viewStart.getTableModel().removeRow(i);
+//		}
+		while(viewStart.getTableModel().getRowCount() > 0){
+			viewStart.getTableModel().removeRow(0);
+		}
+		if ( filtered == null )
+			viewStart.getTableModel().addRow( new String[] { "Keine Einträge für Ihr Suchkriterium vorhanden" } );
+		else {
+			for (GeneralInformationModel generalInformationModel : filtered ) {
+				if ( generalInformationModel != null ){
+					viewStart.getTableModel().addRow( new String[] { generalInformationModel.getFolderName() } );
+				}
+			}
+		}
+	}
+	
+	
+}
